ddfe22e7553603acd94ca66a557337e8b04b702e
subil@secondspass.org <subil@secondspass.org>
2024-11-10 17:50:20 -0500

    some more learning

--- main.lua
+++ main.lua
@@ -1,4 +1,14 @@
-function love.draw()
-    love.graphics.rectangle("fill", 100, 200, 50, 80)
+-- LOVE engine: main.lua
+function love.load()
+	x = 100
 end
 
+function love.update(dt)
+	if x < 600 then
+		x = x + 5 * dt
+	end
+end
+
+function love.draw()
+	love.graphics.rectangle("line", x, 200, 50, 80)
+end
